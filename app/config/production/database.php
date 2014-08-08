3af7cb664a983171e9db551990b00e9c3d86a57c
wilzhao <wzhaomt454@gmail.com>
2014-08-08 14:13:43 -0400

    hopefully works

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => '127.12.224.130',
+            'database'  => 'p4',
+            'username'  => 'adminnZZwwM2',
+            'password'  => 'Ft6DnP3IC4rl',
+        ),
+
+    ),
+
+);
